c3847ab9b99f64484abf0db860ed6374e65b423e
Kaqtus14 <kaqtus14@gmail.com>
2022-06-13 14:31:01 +0200

    Add CLI

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+import argparse
+import subprocess
 import os
-import sys
 
 from ctx import Context
 from lexer import Lexer
@@ -19,18 +20,57 @@ def compile(ctx):
     return interpreter.compile(statements)
 
 
-def main():
-    with open(sys.argv[1]) as f:
-        ctx = Context(sys.argv[1],f.read())
+def compile_file(args):
+    include_path = os.path.join(os.path.dirname(__file__), "..", "stdlib")
+
+    with open(args.path) as f:
+        ctx = Context(args.path, f.read())
     out = compile(ctx)
 
-    print(out)
-    with open("output.cpp", "w+") as f:
+    with open(args.build_path, "w+") as f:
         f.write(out)
 
-    include_path = os.path.join(os.path.dirname(__file__), "..", "stdlib")
-    if os.system(f"g++ -I {include_path} -o output.exe output.cpp"):
+    if os.system(f"g++ -I {include_path} -o {args.out_path} {args.build_path}"):
         exit(1)
+
+    if args.run:
+        os.system(args.out_path if args.out_path.startswith(
+            "/") else "./"+args.out_path)
+
+
+def run_tests(args):
+    for file in os.listdir("examples/"):
+        proc = subprocess.Popen("./aiur c -r examples/"+file,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        proc.wait()
+        if proc.returncode == 0:
+            print(f"✅ {file} {b''.join(proc.communicate())[:100]}")
+        else:
+            print(b"\n".join(proc.communicate()).decode().strip())
+            print(f"❌ {file}")
+            exit(1)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="aiur", usage="%(prog)s <mode>", description="Aiur compiler")
+    subparsers = parser.add_subparsers(dest="mode", required=True)
+
+    compile_parser = subparsers.add_parser("compile", aliases=["c"])
+    compile_parser.add_argument(
+        "-r", dest="run", action="store_true", help="Run executable after compiling")
+    compile_parser.add_argument(
+        "-b", dest="build_path", default="/tmp/aiur_build.cpp", help="Specify the build path (default: /tmp/aiur_build.cpp)")
+    compile_parser.add_argument(
+        "-o", dest="out_path", default="output.exe", help="Specify the out path (default: output.exe)")
+    compile_parser.add_argument("path")
+    compile_parser.set_defaults(func=compile_file)
+
+    test_parser = subparsers.add_parser("test")
+    test_parser.set_defaults(func=run_tests)
+
+    args = parser.parse_args()
+    args.func(args)
 
 
 if __name__ == "__main__":
